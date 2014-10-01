0cf7cfa70c9f95158af30076ede7d816472517c5
David Vaz <david.vaz@cuelogic.co.in>
2014-10-01 15:42:21 +0530

    pass & call next method in request handlers

--- static_pages.js
+++ static_pages.js
@@ -1,12 +1,15 @@
-exports.home=function(req,response){
+exports.home=function(req,response,next){
   response.status(200).send("Welcome Home!");
+  next();
 }
 
-exports.about=function(req,response){
+exports.about=function(req,response,next){
   response.status(200).send("This page is About Us!");
+  next();
 }
 
-exports.test=function(req,response){
+exports.test=function(req,response,next){
   response.status(200).send("Path : "+req.custom_path+" &  QueryString : "+req.queryString);
+  next();
 }
 
